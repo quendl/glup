054d2066dd4edc7cce0e96d3c913dbf231671b37
vKxni <Konradtoesch@icloud.com>
2022-06-18 19:54:00 +0200

    version update

--- mix.exs
+++ mix.exs
@@ -4,8 +4,8 @@ defmodule Glup.MixProject do
   def project do
     [
       app: :glup,
-      version: "0.1.0",
-      elixir: "~> 1.12",
+      version: "1.5.0",
+      elixir: "~> 1.13.3",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
