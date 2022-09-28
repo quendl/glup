0f5a0762667df95f68c5453e9b30d5fc15c05e70
Konrad Koni <eventbeate@gmail.com>
2022-09-28 19:51:36 +0200

    chg api

--- lib/glup_web/location/location_info.ex
+++ lib/glup_web/location/location_info.ex
@@ -1,12 +1,20 @@
 defmodule GlupWeb.Location.Info do
   require Logger
 
+  # helper function for the connection
   def user_location(conn) do
     conn
     |> ip()
     |> url()
     |> fetch_location()
   end
+
+  @doc """
+  Here we are using an APi to fetch the location of the user, based on the IP address.
+
+  - https://hexdocs.pm/httpoison/HTTPoison.html
+  - https://hexdocs.pm/elixir/1.12/Map.html
+  """
 
   defp fetch_location(url) do
     with {:ok, %HTTPoison.Response{body: body}} <- HTTPoison.get(url) do
@@ -36,7 +44,7 @@ defmodule GlupWeb.Location.Info do
       ip = to_string(:inet_parse.ntoa(remote_ip))
 
       if ip == "127.0.0.1" do
-        "62.47.244.0"
+        "52.155.68.130"
       else
         ip
       end
